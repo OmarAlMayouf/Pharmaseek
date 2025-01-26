ce563f473b1ae90f52019ba02b43a97a26008000
Omar Al Mayouf <omaralmayouf@gmail.com>
2025-01-26 15:07:35 +0300

    reactNative+tailwind initialization

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white font-extrabold text-3xl">Pharmaseek</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
