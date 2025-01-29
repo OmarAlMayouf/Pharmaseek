0d5c9d67eaed68e6d4892e856d4b633e38f7ae95
faresdu <443102276@student.ksu.edu.sa>
2025-01-30 00:45:00 +0300

    Sign up UI Completed

--- constants/images.js
+++ constants/images.js
@@ -1,11 +1,13 @@
 import investegator from '../assets/images/investegator.png';
 import wavyShape from '../assets/images/wavyShape.png';
 import wavyShape_signIn from '../assets/images/wavyShape-signIn.png';
+import wavyShape_signUp from '../assets/images/wavyShape-signUp.png'
 
 const images = {
     investegator, 
     wavyShape,
     wavyShape_signIn,
+    wavyShape_signUp,
 };
 
 export default images;
