a0e47b773985f621472a8d3810b7ad521c6bba59
Omar Al Mayouf <omaralmayouf@gmail.com>
2025-01-30 21:20:10 +0300

    more page done

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,17 +1,11 @@
-import { View, Text, Image } from 'react-native';
-import { Tabs, Redirect } from 'expo-router';
+import { View, Text } from 'react-native';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-import { icons } from '../../constants';
-
-const TabIcon = ({ icon, color, name, focused }) => {
+const TabIcon = ({ iconName, color, name, focused }) => {
   return (
-    <View className="items-center justify-center gap-2">
-      <Image
-        source={icon}
-        resizeMode="contain"
-        tintColor={color}
-        className="w-9 h-9 mt-9"
-      />
+    <View className="items-center gap-2 mt-4">
+      <Ionicons name={`${focused? `${iconName}` : `${iconName}-outline`}`} size={28} color={color} />
       <Text 
         className={`${focused ? 'font-rsemibold' : 'font-rregular'} text-sm text-center`}
         style={{ color: color, width: 100 }}
@@ -20,109 +14,106 @@ const TabIcon = ({ icon, color, name, focused }) => {
         {name}
       </Text>
     </View>
-  )
-}
+  );
+};
 
 const tabsLayout = () => {
   return (
-    <>
-      <Tabs
-        screenOptions={{ 
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: '#154C79',
-          tabBarInactiveTintColor: '#7D7D7D',
-          tabBarStyle: {
-            backgroundColor: '#ffffff',
-            shadowColor: '#000000',
-            shadowOffset: {
-              width: 0,
-              height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 8.00,
-            height: 85,
-          }
-        }}
-      >
-        <Tabs.Screen 
-          name="home"
-          options={{
-            title: 'Home',
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon 
-                icon={icons.home}
-                color={color} 
-                name="Home" 
-                focused={focused} 
-              />
-            )
-          }} 
-        />
-        <Tabs.Screen 
-          name="watchlist"
-          options={{
-            title: 'Watchlist',
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon 
-                icon={icons.heart}
-                color={color} 
-                name="Watchlist" 
-                focused={focused} 
-              />
-            )
-          }} 
-        />
-        <Tabs.Screen 
-          name="map"
-          options={{
-            title: 'Map',
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon 
-                icon={icons.pin}
-                color={color} 
-                name="Map" 
-                focused={focused} 
-              />
-            )
-          }} 
-        />
-        <Tabs.Screen 
-          name="reminders"
-          options={{
-            title: 'Reminders',
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon 
-                icon={icons.calendar}
-                color={color} 
-                name="Reminders"
-                focused={focused} 
-              />
-            )
-          }} 
-        />
-        <Tabs.Screen 
-          name="more"
-          options={{
-            title: 'More',
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon 
-                icon={icons.more}
-                color={color} 
-                name="More" 
-                focused={focused} 
-              />
-            )
-          }} 
-        />
-        
-      </Tabs>
-    </>
-  )
-}
+    <Tabs
+      screenOptions={{ 
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: '#154C79',
+        tabBarInactiveTintColor: '#7D7D7D',
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+          shadowColor: '#000000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 8.00,
+          height: 85,
+        }
+      }}
+    >
+      <Tabs.Screen 
+        name="home"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon 
+              iconName="home"
+              color={color} 
+              name="Home" 
+              focused={focused} 
+            />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="watchlist"
+        options={{
+          title: 'Watchlist',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon 
+              iconName="heart"
+              color={color} 
+              name="Watchlist" 
+              focused={focused} 
+            />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="map"
+        options={{
+          title: 'Map',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon 
+              iconName="location"
+              color={color} 
+              name="Map" 
+              focused={focused} 
+            />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="reminders"
+        options={{
+          title: 'Reminders',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon 
+              iconName="calendar"
+              color={color} 
+              name="Reminders"
+              focused={focused} 
+            />
+          )
+        }} 
+      />
+      <Tabs.Screen 
+        name="more"
+        options={{
+          title: 'More',
+          headerTitle: 'More',
+          headerTitleAlign: 'center',
+          headerStyle: { shadowOpacity: 0 },
+          headerShown: true,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon 
+              iconName="ellipsis-horizontal"
+              color={color} 
+              name="More" 
+              focused={focused} 
+            />
+          )
+        }} 
+      />
+    </Tabs>
+  );
+};
 
-export default tabsLayout
+export default tabsLayout;
--- app/(tabs)/more.jsx
+++ app/(tabs)/more.jsx
@@ -1,12 +1,98 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 const more = () => {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>more</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default more
+  const menuItems = [
+    { title: "My Profile", icon: "person-outline", route: "Profile" },
+    { title: "Addresses", icon: "location-outline", route: "Addresses" },
+    { title: "Support", icon: "headset-outline", route: "Support" },
+  ];
+
+  const settingsItems = [
+    { title: "Settings", icon: "settings-outline", route: "Settings" },
+    { title: "FAQs", icon: "help-circle-outline", route: "FAQs" },
+    { title: "About us", icon: "information-circle-outline", route: "About" },
+    {
+      title: "Terms and conditions",
+      icon: "document-text-outline",
+      route: "Terms",
+    },
+    {
+      title: "Privacy policy",
+      icon: "shield-checkmark-outline",
+      route: "Privacy",
+    },
+  ];
+
+  return (
+    <ScrollView className="flex-1 bg-white p-4">
+      <View className="mt-6" />
+      <Text className="text-[17px] mb-4 font-rsemibold">My Account</Text>
+      {menuItems.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          className="flex-row items-center justify-between py-3 border-b border-gray-200"
+          onPress={() => navigation.navigate(item.route)}
+          activeOpacity={0.3}
+        >
+          <View className="flex-row items-center">
+            <Ionicons
+              name={item.icon}
+              size={24}
+              className="mr-3"
+              color="#154C79"
+            />
+            <Text className="text-base font-rregular">{item.title}</Text>
+          </View>
+          <Ionicons name="chevron-forward-outline" size={24} color="#7D7D7D" />
+        </TouchableOpacity>
+      ))}
+      <View className="mt-6" />
+      <Text className="text-[17px] mt-4 mb-4 font-rsemibold">App Settings</Text>
+      {settingsItems.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          className="flex-row items-center justify-between py-3 border-b border-gray-200"
+          onPress={() => navigation.navigate(item.route)}
+          activeOpacity={0.3}
+        >
+          <View className="flex-row items-center">
+            <Ionicons
+              name={item.icon}
+              size={24}
+              className="mr-3"
+              color="#154C79"
+            />
+            <Text className="text-base font-rregular">{item.title}</Text>
+          </View>
+          <Ionicons name="chevron-forward-outline" size={24} color="#7D7D7D" />
+        </TouchableOpacity>
+      ))}
+      <View className="mt-6" />
+      <View className="flex-row items-center justify-between mt-4">
+        <Text className="text-sm text-gray-500 font-rregular">
+          App version 5.03
+        </Text>
+        <TouchableOpacity
+          className="flex-row items-center"
+          onPress={() => router.push("/sign-in")}
+          activeOpacity={0.3}
+        >
+          <Ionicons name="log-out-outline" size={30} color="#154C79" />
+          <Text className="text-base text-black ml-2 font-rregular">
+            Log out
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="dark" />
+    </ScrollView>
+  );
+};
+
+export default more;
